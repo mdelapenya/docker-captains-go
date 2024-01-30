dbb08c5ee27f8686297b0e8cb663a43ef584b7e7
Manuel de la Peña <mdelapenya@gmail.com>
2024-01-30 13:54:55 +0100

    feat: enable dev mode for POstgrews using testcontainers-go

---
+++ dev_mode.go
@@ -0,0 +1,83 @@
+//go:build dev
+// +build dev
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func init() {
+	ctx := context.Background()
+
+	c, err := postgres.RunContainer(ctx,
+		testcontainers.WithImage("postgres:15.3-alpine"),
+		postgres.WithInitScripts(filepath.Join(".", "testdata", "schema.sql")),
+		postgres.WithDatabase("todos"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).WithStartupTimeout(5*time.Second)),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	connStr, err := c.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	// check the connection to the database
+	conn, err := pgx.Connect(ctx, connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	App.UsersConnection = connStr
+	log.Println("Users database started successfully")
+
+	// register a graceful shutdown to stop the dependencies when the application is stopped
+	// only in development mode
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+	go func() {
+		// also use the shutdown function when the SIGTERM or SIGINT signals are received
+		sig := <-gracefulStop
+		fmt.Printf("caught sig: %+v\n", sig)
+		err := shutdownDependencies(c)
+		if err != nil {
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}()
+}
+
+// helper function to stop the dependencies
+func shutdownDependencies(containers ...testcontainers.Container) error {
+	ctx := context.Background()
+	for _, c := range containers {
+		err := c.Terminate(ctx)
+		if err != nil {
+			log.Println("Error terminating the backend dependency:", err)
+			return err
+		}
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -12,11 +12,23 @@ type Todo struct {
 	ID        string `json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
-	Order     int    `json:"order"`
+	Order     int    `json:"order_number"`
 }
 
 func (t *Todo) URL() string {
 	return fmt.Sprintf("/todos/%s", t.ID)
+}
+
+type TodoApp struct {
+	Name            string
+	Version         string
+	UsersConnection string
+}
+
+var App = &TodoApp{
+	Name:            "Todos",
+	Version:         "1.0.0",
+	UsersConnection: os.Getenv("POSTGRESQL_URL"),
 }
 
 func main() {
@@ -27,7 +39,7 @@ func main() {
 	// register handler for the static files
 	mux.Handle("/", FileSystemHandler)
 
-	store, err := NewTodosRepository(context.Background(), os.Getenv("POSTGRESQL_URL"))
+	store, err := NewTodosRepository(context.Background(), App.UsersConnection)
 	if err != nil {
 		log.Fatalf("Cannot create a Todos repository. Exiting")
 	}
