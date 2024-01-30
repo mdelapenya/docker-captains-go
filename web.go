844815305a7773ae2df9d971c63936c995388420
Manuel de la Peña <mdelapenya@gmail.com>
2024-01-30 13:37:08 +0100

    feat: create custom handler errors

--- web.go
+++ web.go
@@ -4,6 +4,16 @@ import "net/http"
 
 var FileSystemHandler http.Handler = http.StripPrefix("/", http.FileServer(http.Dir("public")))
 
+func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 Internal Server Error"))
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("404 Not Found"))
+}
+
 type TodosHandler struct {
 	store todoStore
 }
