f210a74124bf1c4cfb028fc4e62090e25d19ec45
Manuel de la Peña <mdelapenya@gmail.com>
2024-01-30 13:56:18 +0100

    feat: implement the create handler

--- web.go
+++ web.go
@@ -1,6 +1,16 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+var (
+	TodoRe       = regexp.MustCompile(`^/todos/*$`)
+	TodoReWithID = regexp.MustCompile(`^/todos/([a-z0-9]+(?:-[a-z0-9]+)+)$`)
+)
 
 var FileSystemHandler http.Handler = http.StripPrefix("/", http.FileServer(http.Dir("public")))
 
@@ -25,5 +35,52 @@ func NewTodosHandler(s todoStore) *TodosHandler {
 }
 
 func (h *TodosHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is my todos page"))
+	switch {
+	case r.Method == http.MethodPost && TodoRe.MatchString(r.URL.Path):
+		h.Create(w, r)
+		return
+	case r.Method == http.MethodGet && TodoRe.MatchString(r.URL.Path):
+		h.List(w, r)
+		return
+	case r.Method == http.MethodGet && TodoReWithID.MatchString(r.URL.Path):
+		h.FindByID(w, r)
+		return
+	case r.Method == http.MethodPut && TodoReWithID.MatchString(r.URL.Path):
+		h.Update(w, r)
+		return
+	case r.Method == http.MethodDelete && TodoReWithID.MatchString(r.URL.Path):
+		h.Delete(w, r)
+		return
+	case r.Method == http.MethodDelete && TodoRe.MatchString(r.URL.Path):
+		h.DeleteAll(w, r)
+		return
+	default:
+		return
+	}
 }
+
+func (h *TodosHandler) Create(w http.ResponseWriter, r *http.Request) {
+	// Todo object that will be populated from JSON payload
+	var todo Todo
+	if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+		log.Printf("Cannot decode the request body: %v", err)
+		InternalServerErrorHandler(w, r)
+		return
+	}
+
+	// Call the store to add the recipe
+	if err := h.store.Create(r.Context(), &todo); err != nil {
+		log.Printf("Cannot create the todo: %v", err)
+		InternalServerErrorHandler(w, r)
+		return
+	}
+
+	// Set the status code to 200
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *TodosHandler) List(w http.ResponseWriter, r *http.Request)      {}
+func (h *TodosHandler) FindByID(w http.ResponseWriter, r *http.Request)  {}
+func (h *TodosHandler) Update(w http.ResponseWriter, r *http.Request)    {}
+func (h *TodosHandler) Delete(w http.ResponseWriter, r *http.Request)    {}
+func (h *TodosHandler) DeleteAll(w http.ResponseWriter, r *http.Request) {}
