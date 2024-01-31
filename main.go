3a32a730588c3c7c9a502e53f225a649b3d67144
Manuel de la Peña <mdelapenya@gmail.com>
2024-01-31 14:22:27 +0100

    fix: proper json field for order

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type Todo struct {
 	ID        string `json:"id"`
 	Title     string `json:"title"`
 	Completed *bool  `json:"completed"`
-	Order     *int   `json:"order_number"`
+	Order     *int   `json:"order"`
 	Url       string `json:"url"`
 }
 
